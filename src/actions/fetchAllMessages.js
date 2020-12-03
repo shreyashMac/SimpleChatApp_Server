5bb670e8b58335aeb8a7ae0d06fc8438f6fae9fa
Shreyas Prakashbhai Macwan <shreyasmacwan@Shreyass-MacBook-Pro.local>
2020-12-03 16:26:10 -0500

    Encryption and Decryption of the text messages added

--- src/actions/fetchAllMessages.js
+++ src/actions/fetchAllMessages.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 const Chat = mongoose.model("Chat");
+const _ = require("lodash");
+const Cryptr = require("cryptr");
+const cryptr = new Cryptr("thisIsConfidential");
+
 const fetchAllMessages = async () => {
   try {
     const chatData = await Chat.find({}).sort({ createdAt: -1 });
--- src/models/Chat.js
+++ src/models/Chat.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+const _ = require("lodash");
+const Cryptr = require("cryptr");
+const KEY = require("../constants/key");
+const cryptr = new Cryptr(KEY);
 
 const chatSchema = new mongoose.Schema({
   text: String,
@@ -7,6 +11,19 @@ const chatSchema = new mongoose.Schema({
   },
   _id: String,
   createdAt: String,
+});
+
+chatSchema.pre("save", function () {
+  console.log("Inside the pre function");
+  const Chat = this;
+  Chat.text = cryptr.encrypt(Chat.text);
+});
+
+chatSchema.post("find", function (docs) {
+  _.map(docs, (data) => {
+    data.text = cryptr.decrypt(data.text);
+    return data;
+  });
 });
 
 mongoose.model("Chat", chatSchema);
